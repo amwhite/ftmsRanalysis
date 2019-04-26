3bba3ffc760dc013104040a643a03cf158f700ef
Amanda White <amanda.white@pnnl.gov>
2019-04-25 18:31:07 -0700

    many documentation changes to satisfy R CMD check

--- R/vanKrevelenCognostics.R
+++ R/vanKrevelenCognostics.R
@@ -7,13 +7,13 @@
 #' and (for \code{comparisonSummary} objects only) the name of the column to use
 #' for identifying which peaks are observed in which group. It 
 #' returns a function that may be applied to each \code{ftmsData} object, as is
-#' appropriate for use with the \code{\link{makeDisplay}} function. See 
+#' appropriate for use with the \code{\link[trelliscope:makeDisplay]{makeDisplay}} function. See 
 #' Examples section for use.
 #'
 #' @param vkBoundarySet Van Krevelen boundary set to use for calculating class proportions
 #' @param uniquenessColName if \code{ftmsObj} is a group comparison summary object, what is the 
 #' name of the column that specifies uniqueness to a group? If only one uniqueness function has
-#' been applied this is unnecessary. (See \code{\link{summarizeComparisons}}.)
+#' been applied this is unnecessary. (See \code{\link{summarizeGroupComparisons}}.)
 #'
 #' @return a function that may be applied to objects of type \code{peakData}, \code{groupSummary},
 #' and \code{comparisonSummary}
